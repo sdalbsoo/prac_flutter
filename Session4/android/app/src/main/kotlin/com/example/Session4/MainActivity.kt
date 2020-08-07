6541dabbd7934644407ad44b65b41ec9826ee157
SHIN BEOMSOO <sdalbsoo@gmail.com>
2020-08-07 15:45:39 +0900

    20200807 85

---
+++ Session4/android/app/src/main/kotlin/com/example/Session4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Session4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Session4/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './widgets/user_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleControler = TextEditingController();
+  final amountControler = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+              child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 10,
+              ),
+            ),
+            UserTransactions(),
+          ],
+        ),
+      ),
+    );
+  }
+}
